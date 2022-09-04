6242251c2fc86e20f18b26ee5a86b03f1a19c77a
irfan <23pointalpha@gmail.com>
2022-09-04 21:08:40 +0300

    Day5 DBUtils added

--- src/test/java/com/cydeo/day5/JSONtoJAVATest.java
+++ src/test/java/com/cydeo/day5/JSONtoJAVATest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -32,6 +33,24 @@ public class JSONtoJAVATest extends SpartanTestBase {
         //after we got the map, we can use Hamcrest or Junit assertions to do assertion
         String actualName = (String) jsonMap.get("name");
         assertThat(actualName, is("Meta"));
+    }
+
+    @DisplayName("GET all spartans to JAVA data structure")
+    @Test
+    public void getAllSpartan(){
+        Response response = get("/api/spartans").then().statusCode(200).extract().response();
+
+        //we need to convert json to java which is deserialize
+
+        List<Map<String,Object>> jsonList = response.as(List.class);
+        //after this part is java, not API
+
+        System.out.println("jsonList.get(1).get(\"name\") = " + jsonList.get(1).get("name"));
+        //jsonList.get(1) --> json objects index 1 (second json object)
+        //jsonList.get(1).get("name") --> second json object (second map), key "name"
+
+        Map<String,Object> spartan3 = jsonList.get(2);
+        System.out.println(spartan3);
     }
 
 }
