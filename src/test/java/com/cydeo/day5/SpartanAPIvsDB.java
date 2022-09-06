18910f8d1bef2a5a30e829f24d6a49521baa0fa6
irfan <23pointalpha@gmail.com>
2022-09-06 14:14:05 +0300

    Day6 pojo - Search.java added in pojo for SpartanPojoGetRequestTest.java test3() method - test4() is the way without Search pojo

--- src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
+++ src/test/java/com/cydeo/day5/SpartanAPIvsDB.java
@@ -13,6 +13,7 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+
 public class SpartanAPIvsDB extends SpartanTestBase {
     @DisplayName("GET one spartan from api and database")
     @Test
